9ac531694d72df9f6d5d00b3d63f944096e6a69f
Eric Timmerman <estimmerman@gmail.com>
2018-11-13 17:18:36 -0500

    setup

---
+++ js/examplevis.js
@@ -0,0 +1,83 @@
+/*
+ * ExampleVis - Object constructor function
+ * @param _parentElement 	-- the HTML element in which to draw the visualization
+ * @param _data	    		-- the actual data: perDayData
+ * @param _eventHandler     -- event handler for this visualization (perhaps not necessary if a vis only reacts to, not triggers, events)
+ */
+ExampleVis = function(_parentElement, _data, _eventHandler) {
+    this.parentElement = _parentElement;
+    this.data = _data;
+    this.eventHandler = _eventHandler;
+    this.filteredData = this.data;
+
+    this.initVis();
+};
+
+/*
+ * Initialize visualization (static content, e.g. SVG area or axes)
+ */
+
+ExampleVis.prototype.initVis = function() {
+    var vis = this;
+
+    vis.margin = { top: 40, right: 40, bottom: 40, left: 40 };
+
+    vis.width = $("#" + vis.parentElement).width() - vis.margin.left - vis.margin.right,
+        vis.height = 300 - vis.margin.top - vis.margin.bottom;
+
+    // SVG drawing area
+    vis.svg = d3.select("#" + vis.parentElement).append("svg")
+        .attr("width", vis.width + vis.margin.left + vis.margin.right)
+        .attr("height", vis.height + vis.margin.top + vis.margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + vis.margin.left + "," + vis.margin.top + ")");
+
+    // Scales, axes, and domains
+
+    // (Filter, aggregate, modify data)
+    vis.wrangleData();
+};
+
+/*
+ * Data wrangling
+ */
+
+ExampleVis.prototype.wrangleData = function() {
+    var vis = this;
+
+    // Perform any data manipulation if necessary on the filtered data
+    vis.displayData = vis.filteredData;
+
+    // Update the visualization
+    vis.updateVis();
+};
+
+/*
+ * The drawing function - should use the D3 update sequence (enter, update, exit)
+ * Function parameters only needed if different kinds of updates are needed
+ */
+
+ExampleVis.prototype.updateVis = function() {
+    var vis = this;
+
+    /* Draw vis using vis.displayData */
+
+    // Enter, update, exit
+
+    // Draw axes (if applicable)
+
+    // Draw legend (if applicable)
+};
+
+/*
+ * Filtering function - only necessary if this visualization is filtered based on some mechanic
+ * E.g. brush, inputs, event trigger from another visualization in the dashboard (apply parameters as necessary)
+ */
+
+ExampleVis.prototype.onUpdateData = function() {
+    var vis = this;
+
+    // Update vis.filteredData
+
+    vis.wrangleData();
+};
---
+++ js/main.js
@@ -0,0 +1,37 @@
+/* Parsers */
+// TODO
+
+/* Load data */
+queue()
+    .await(createVis);
+
+function createVis(error) {
+    if (error) { console.log(error); }
+
+    /*** Clean data ***/
+    // TODO
+
+    /*** Create dashboards ***/
+
+    // Dashboard 1
+    // TODO
+
+    /* Create visualization instances */
+    // e.g. var exampleVis = new ExampleVis("example-vis", data, eventHandler);
+
+    /* Bind event handlers */
+
+    // Dashboard 2
+    // TODO
+
+    /* Create visualization instances */
+
+    /* Bind event handlers */
+
+    // Dashboard 3
+    // TODO
+
+    /* Create visualization instances */
+
+    /* Bind event handlers */
+}
